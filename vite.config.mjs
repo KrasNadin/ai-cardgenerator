902d1bd2a8d50457478788559c33e23006845d27
nadezdachepikova <nedezdachepikova@mail.com>
2025-06-18 18:10:42 +0500

    fix: add base path for GitHub Pages

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,27 +1,26 @@
 import { defineConfig, loadEnv } from 'vite'
-
 import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint';
 import path from 'path';
 
 export default () => {
-    return defineConfig(
-        {
-            plugins: [
-                react(),
-                // eslint({
-                //   include: ['./src/**/*.{js,jsx,ts,tsx}'],
-                //   failOnError: false,
-                //   exclude: ['./node_modules']
-                // })
-            ],
-            server: {
-                open: '/',
-                port: 3000,
-            },
-            resolve: {
-                alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
-            },
-        }
-    )
+    return defineConfig({
+        base: '/ankipower-cardgenerator/',
+
+        plugins: [
+            react(),
+            // eslint({
+            //   include: ['./src/**/*.{js,jsx,ts,tsx}'],
+            //   failOnError: false,
+            //   exclude: ['./node_modules']
+            // })
+        ],
+        server: {
+            open: '/',
+            port: 3000,
+        },
+        resolve: {
+            alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+        },
+    });
 }
